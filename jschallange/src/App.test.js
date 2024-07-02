03d1b538532dd781e8d7309b2c0408f0d917bf6b
yimajiuni <106362564+yimajiuni@users.noreply.github.com>
2024-07-02 15:03:00 +0900

    Deleted 0701

--- jschallange/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import StateTutorial from "./StateTutorial";
-
-test("renders learn react link", () => {
-  render(<StateTutorial />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
