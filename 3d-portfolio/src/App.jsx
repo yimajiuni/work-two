151bc73c8dadf3876235f951d5f2371be0411068
yimajiuni <106362564+yimajiuni@users.noreply.github.com>
2024-07-29 15:17:45 +0900

    commit0729

--- 3d-portfolio/src/App.jsx
+++ 3d-portfolio/src/App.jsx
@@ -2,31 +2,49 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import {
   Home,
-  About,
-  Projects,
   Contact,
   TranslatedProjects,
   TranslatedAbout,
   TranslatedDesigns,
 } from "./pages";
 import Navbar from "./components/Navbar";
+{
+  /*import NavbarJump from "./components/NavbarJump";*/
+}
 
 function App() {
   return (
-    <main className="bg-pink-300/40 h-full">
+    <div className="bg-pink-300/40 h-full">
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<TranslatedAbout />} />
-          {/*<Route path="/about" element={<About />} />*/}
           <Route path="/projects" element={<TranslatedProjects />} />
-          {/*<Route path="/projects" element={<Projects />} />*/}
           <Route path="/designs" element={<TranslatedDesigns />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-    </main>
+      {/*}
+      <NavbarJump />
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+        <div id="about">
+          <TranslatedAbout />
+        </div>
+        <div id="projects">
+          <TranslatedProjects />
+        </div>
+        <div id="designs">
+          <TranslatedDesigns />{" "}
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+      </main>*/}
+    </div>
   );
 }
 
