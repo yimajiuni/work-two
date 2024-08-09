ce2dd8125ff1e48d91b1f47a3c817c67b0ae1e36
yimajiuni <106362564+yimajiuni@users.noreply.github.com>
2024-08-09 12:33:19 +0900

    commit0809

--- blog-app/src/components/categorylist/CategoryList.jsx
+++ blog-app/src/components/categorylist/CategoryList.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import styles from "./categoryList.module.css";
-import Link from "next/link";
-import Image from "next/image";
+import React from 'react'
+import styles from './categoryList.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 /*fetch actual data*/
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
-    cache: "no-store",
-  });
+  const res = await fetch('http://localhost:3000/api/categories', {
+    cache: 'no-store',
+  })
 
   if (!res.ok) {
-    throw new Error("Failed");
+    throw new Error('Failed')
   }
 
-  return res.json();
-};
+  return res.json()
+}
 
 const CategoryList = async () => {
-  const data = await getData();
+  const data = await getData()
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
@@ -31,7 +31,7 @@ const CategoryList = async () => {
             {item.img && (
               <Image
                 src={item.img}
-                alt=""
+                alt=''
                 width={32}
                 height={32}
                 className={styles.image}
@@ -42,7 +42,7 @@ const CategoryList = async () => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CategoryList;
+export default CategoryList
